672295f0066a7fed1b1ff6c7458fbd259b38664d
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2022-04-11 22:33:09 +0700

    fix: gm_getvalue not return base value

--- src/scripts/models/GM_Library.js
+++ src/scripts/models/GM_Library.js
@@ -4,9 +4,15 @@ export default class GM_Library extends BaseModel {
     constructor() {
         super();
     }
+    /**
+     * Get current app information
+     */
     get appInfo() {
         return this.getInfo("script");
     }
+    /**
+     * Get current userscript manager name
+     */
     get managerName() {
         if (typeof GM_info === "object") {
             // Greasemonkey (Firefox)
@@ -35,12 +41,23 @@ export default class GM_Library extends BaseModel {
             }
         }
     }
+    /**
+     * Get some app informations using keyname
+     * @param {String} key keyname
+     * @returns
+     */
     getInfo(key = "") {
         if (!GM_info) {
             return null;
         }
         return key === "" ? GM_info : this.findObjectValue(key, GM_info);
     }
+    /**
+     * Get app resource
+     * @param {String} key name of the resource
+     * @param {String} mode type of resource (text | url)
+     * @returns
+     */
     getResource(key, mode = "text") {
         if (typeof GM_getResourceText !== "undefined" && mode === "text") {
             return GM_getResourceText(key);
@@ -48,6 +65,10 @@ export default class GM_Library extends BaseModel {
             return GM_getResourceURL(key);
         }
     }
+    /**
+     * Delete value on local storage (not implemented yet)
+     * @param {String} name value name
+     */
     deleteValue(name) {
         if (typeof GM_deleteValue !== "undefined") {
             const vals = GM_listValues();
@@ -60,11 +81,22 @@ export default class GM_Library extends BaseModel {
             }
         }
     }
+    /**
+     * Set and save some value to local storage
+     * @param {String} name what name
+     * @param {any} value what value
+     */
     setValue(name, value) {
         if (typeof GM_setValue !== "undefined") {
             GM_setValue(name, value);
         }
     }
+    /**
+     * Get value from local storage using it's name as keyword
+     * @param {String} name what name
+     * @param {any} base set base value if not found. Difault is null
+     * @returns {any}
+     */
     getValue(name, base = null) {
         if (typeof GM_listValues !== "undefined" && typeof GM_getValue !== "undefined") {
             const keys = GM_listValues();
@@ -73,10 +105,13 @@ export default class GM_Library extends BaseModel {
                     return GM_getValue(name, base);
                 }
             }
-        } else {
-            return base;
         }
+        return base;
     }
+    /**
+     * GM XMLHttpRequest Implementation
+     * @param {Tampermonkey.Request} options
+     */
     request(options) {
         options.url = options.url || this.appInfo.homepage;
         options.method = options.method || "GET";
@@ -100,15 +135,15 @@ export default class GM_Library extends BaseModel {
             _onload(xhr);
         };
         options.onerror = function (xhr) {
-            console.log(xhr);
+            console.log(`request error: ${xhr.error || "unknown error"}`);
             _onerror(xhr);
         };
         options.ontimeout = function (xhr) {
-            console.log(xhr);
+            console.log(`request error: ${xhr.error || "timed out"}`);
             _ontimeout(xhr);
         };
         options.onabort = function (xhr) {
-            console.log(xhr);
+            console.log(`request error: ${xhr.error || "aborted"}`);
             _onabort(xhr);
         };
 
