377d5ccd019a8b14435f67e6b2f7196b823ebe79
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2022-04-11 22:34:09 +0700

    feat: add jsonparse validator

--- src/scripts/lib/Util.js
+++ src/scripts/lib/Util.js
@@ -1,30 +1,74 @@
 import MyArray from "../models/MyArray";
 import { rgx, dateOptDefault } from "./Constant";
 
-const useComa = (text) => {
+/**
+ * Scan and determine csv delimiter. Using comma or not.
+ * @param {String} text CSV row
+ * @returns {Boolean}
+ */
+const useComma = (text) => {
     let pattern = String.raw`^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,;"\s\\]*(?:\s+[^,;"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,;"\s\\]*(?:\s+[^,;"\s\\]+)*)\s*)*$`,
         regExp = new RegExp(pattern);
     return regExp.test(text);
 };
 
+/**
+ * Check and detect the string is numeric or not
+ * @param {String} str input string
+ * @returns {Boolean}
+ */
 const isNumeric = (str) => {
     return typeof str === "string"
         ? !isNaN(str) && !isNaN(parseFloat(str))
         : typeof str === "number";
 };
 
+/**
+ * Check and detect the string is datestring or not
+ * @param {String} str input string
+ * @returns {Boolean}
+ */
 const isDateStr = (str) => {
     return typeof str !== "string"
         ? false
         : new Date(str).toString() !== "Invalid Date" && !isNaN(new Date(str));
 };
 
+/**
+ * Check and detect the string is JSON valid or not.
+ * If it's valid, then return it's JSON value.
+ * If it isn't valid, then return it as null.
+ * @param {string} str JSON string
+ * @returns {Promise<JSON | null>}
+ */
+const JSONParse = (str) => {
+    return new Promise((done) => {
+        try {
+            done(JSON.parse(str));
+        } catch (e) {
+            done(null);
+        }
+    });
+};
+
+/**
+ * Check and get the exact RegExp to get csv row value.
+ * @param {String} d csv delimiter
+ * @param {String} flag
+ * @returns {RegExp}
+ */
 const rowValue = (d = ",", flag = "") => {
     let add = d == "," ? `${d};` : d,
         pattern = String.raw`(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^${add}"\s\\]*(?:\s+[^${add}"\s\\]+)*))\s*(?:${d}|$)`;
     return flag ? new RegExp(pattern, flag) : new RegExp(pattern);
 };
 
+/**
+ * Check current app version is up to date or not.
+ * @param {String} local local/instaled version
+ * @param {String} remote remote/server version
+ * @returns {Boolean}
+ */
 const isUpToDate = (local, remote) => {
     const { forVersion } = rgx;
     if (!local || !remote || local.length === 0 || remote.length === 0) {
@@ -74,11 +118,24 @@ const isUpToDate = (local, remote) => {
     return local >= remote;
 };
 
+/**
+ * Check and get the sign up date based on RegExp pattern.
+ * Return as string if any matched.
+ * Return as null of no matched.
+ * @param {String} date input datestring
+ * @returns {String | null}
+ */
 const getSignDate = (date) => {
     date = rgx.datePattern.exec(date);
     return date ? date.toString() : null;
 };
 
+/**
+ * Get Name or Full Name and change it to Title Case
+ * @param {String} str input name
+ * @param {Boolean} full is it full name?
+ * @returns {String}
+ */
 const setName = (str, full = false) => {
     let name = [];
     str.split(" ").forEach((e) => {
@@ -87,11 +144,21 @@ const setName = (str, full = false) => {
     return full ? name.join(" ") : name[0];
 };
 
+/**
+ * Conver string to Title Case
+ * @param {String} string input string
+ * @returns {String}
+ */
 const titleCase = (string) => {
     string = string.toLowerCase();
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
+/**
+ * Get and convert current phone number
+ * @param {String | String[]} ph input phone
+ * @returns {String}
+ */
 const setPhone = (ph) => {
     ph = typeof ph === "string" ? ph : ph.join("");
     return ph.replace(rgx.phoneValue, function (m0, g1, g2) {
@@ -101,6 +168,12 @@ const setPhone = (ph) => {
     });
 };
 
+/**
+ * Set and print date based on toLocaleDateString format
+ * @param {String | Number | Date} value Date value
+ * @param {Number} i weekday changing. if 0, then print. If other than 0, don't print
+ * @returns {String}
+ */
 const dateFormat = (value, i = 0) => {
     return new Date(value).toLocaleDateString(
         "id-ID",
@@ -110,6 +183,11 @@ const dateFormat = (value, i = 0) => {
     );
 };
 
+/**
+ * Calculate and get month indext on some datestring
+ * @param {String} date input datestring
+ * @returns {0 | 1 | 2}
+ */
 const monthIndex = (date) => {
     if (rgx.formatedDate.test(date[0])) {
         return 2;
@@ -128,6 +206,13 @@ const monthIndex = (date) => {
         : 1;
 };
 
+/**
+ * Detect and get csv row data, then convert it into custom Array.
+ * If it's invalid row, then return false.
+ * @param {String} row CSV row data
+ * @param {String} splitter csv delimiter
+ * @returns {MyArray | False}
+ */
 const isValidRow = (row, splitter) => {
     const data = CSVtoArray(row, splitter),
         validPhone = (val) => rgx.phonePattern.test(val);
@@ -146,6 +231,12 @@ const isValidRow = (row, splitter) => {
     return false;
 };
 
+/**
+ * Just Convert csv data into custom Array.
+ * @param {String} text csv row value
+ * @param {String} delimiter csv delimiter
+ * @returns {MyArray}
+ */
 const CSVtoArray = (text, delimiter = ",") => {
     let data = new MyArray();
     text.replace(rowValue(delimiter, "g"), function (match, g1, g2, g3) {
@@ -162,11 +253,16 @@ const CSVtoArray = (text, delimiter = ",") => {
     return data;
 };
 
+/**
+ * Load file from text based filetype (txt/csv)
+ * @param {Array} data csv|text file array of row data
+ * @returns
+ */
 const loadFile = (data) => {
     const idx = data.length > 1 && data[1] != "" ? 1 : 0,
         newData = new MyArray(),
         dt = new MyArray(),
-        splitter = useComa(data[idx]) ? "," : ";";
+        splitter = useComma(data[idx]) ? "," : ";";
 
     let row, s;
 
@@ -186,7 +282,13 @@ const loadFile = (data) => {
     };
 };
 
-const exportToCsv = (filename, data) => {
+/**
+ * Convert and export an array to csv data. Also create URL to download it.
+ * @param {String} filename
+ * @param {Array} data csv data
+ * @returns
+ */
+const exportToCsv = (filename, type, data) => {
     const processRow = function (row) {
         let finalVal = "";
         for (let j = 0; j < row.length; j++) {
@@ -210,7 +312,16 @@ const exportToCsv = (filename, data) => {
     const blob = new Blob([csvFile], { type: "text/csv;charset=utf-8;" });
     const csvUrl = URL.createObjectURL(blob);
 
-    return { fileUrl: csvUrl, fileName: filename };
+    return { fileUrl: csvUrl, fileName: `${filename}_${type}` };
 };
 
-export { isNumeric, isDateStr, isUpToDate, setName, dateFormat, loadFile, exportToCsv };
+export {
+    isNumeric,
+    isDateStr,
+    isUpToDate,
+    setName,
+    dateFormat,
+    loadFile,
+    exportToCsv,
+    JSONParse,
+};
