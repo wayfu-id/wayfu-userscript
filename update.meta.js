e960190f7a3d27babb0b87383aa7e610b61d7ad8
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2022-05-16 17:42:16 +0700

    chore(build): build & bump new version

--- update.meta.js
+++ update.meta.js
@@ -1,5 +1,34 @@
 // ==UserScript==
 // @name         WayFu - Easy Follow Up
-// @version      4.0.4
+// @description  WhatsApp Easy Follow Up.
+// @date         2022-05-16
+// @copyright    2018, Rizal Nurhidayat (https://github.com/rzlnhd)
+// @license      CC BY-NC-ND 4.0; https://creativecommons.org/licenses/by-nc-nd/4.0/
+// @icon         https://wayfu.id/v4/assets/wayfu-icon.webp
+// @homepage     https://wayfu.id/
+// @supportURL   https://wayfu.id/
+// @version      4.0.5
+// @author       Rizal Nurhidayat (https://github.com/rzlnhd)
+// @match        https://web.whatsapp.com/*
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
+// @grant        GM_log
+// @grant        GM_openInTab
+// @grant        GM_setClipboard
+// @grant        GM_info
+// @grant        GM_notification
+// @connect      wayfu.id
+// @updateURL    https://wayfu.id/v4/update.meta.js
+// @downloadURL  https://wayfu.id/v4/wayfu-install.user.js
+// @resource ico https://wayfu.id/v4/assets/wayfu-icon.webp
+// @resource pnl https://wayfu.id/v4/assets/wayfu-view.html
+// @resource clr https://wayfu.id/v4/assets/wayfu-colors.json
+// @resource css https://wayfu.id/v4/assets/wayfu-style.min.css
 // ==/UserScript==
 
