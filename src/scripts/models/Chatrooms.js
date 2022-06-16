fb4e00d9ecfb50591127045efa639e9ee04dd8eb
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2022-06-16 18:00:52 +0700

    chore: add support to check and collect chatroom data

--- src/scripts/models/Chatrooms.js
+++ src/scripts/models/Chatrooms.js
@@ -2,19 +2,34 @@ import { DOM } from "../lib/DOM";
 
 class Chatroom {
     constructor() {
-        this.chat;
-        this.chatClass = "";
+        this.room = null;
+        this.classId = "";
+    }
+
+    get isGroup() {
+        return this.selected ? this.room.isGroup : false;
+    }
+
+    get groupMetadata() {
+        return this.selected && this.isGroup ? this.room.groupMetadata : null;
+    }
+
+    /**
+     * get contact from active chatroom
+     */
+    get contact() {
+        if (this.room) {
+            return this.room.contact;
+        }
+
+        return null;
     }
 
     /**
      * any selected chatroom?
      */
     get selected() {
-        if (this.chatClass !== "") {
-            this.chat = DOM.getElement(`div.${this.chatClass}`);
-            return !!this.chat;
-        }
-        return false;
+        return !!this.room;
     }
 
     /**
@@ -23,7 +38,23 @@ class Chatroom {
      * @returns
      */
     init(activeClass) {
-        this.chatClass = activeClass;
+        this.classId = activeClass;
+        return this;
+    }
+
+    selectChat() {
+        let elm = this.classId !== "" ? DOM.getElement(`div.${this.classId}`) : null;
+        if (elm !== null) {
+            elm = elm.offsetParent;
+            for (let key of Object.keys(elm)) {
+                let { children } = elm[key];
+                if (children) {
+                    this.room = children.props.chat;
+                    break;
+                }
+            }
+        }
+
         return this;
     }
 }
