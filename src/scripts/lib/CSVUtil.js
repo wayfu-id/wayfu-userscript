ca27f330cb1e61ff0ea6194988a8589f93a872f2
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2022-11-04 14:06:31 +0700

    feat(data): Implement xlsx file reader

--- src/scripts/lib/CSVUtil.js
+++ src/scripts/lib/CSVUtil.js
@@ -1,20 +1,22 @@
 import MyArray from "../models/MyArray";
 import { rgx } from "./Constant";
+import { isNumeric } from "./Util";
 
 /**
  * Calculate and get month indext on some datestring
- * @param {String} date input datestring
+ * @param {string[]} date input datestring
  * @returns {0 | 1 | 2}
  */
 const monthIndex = (date) => {
-    if (rgx.formatedDate.test(date[0])) {
-        return 2;
-    }
+    if (rgx.formatedDate.test(date[0])) return 2;
+
     let a = new MyArray(),
         b = new MyArray();
+
     date.forEach((e, i) => {
         [a[i], b[i]] = e.split(/\/|:|-/);
     });
+
     return a.some((x) => x > 12) || b.some((x) => x > 12)
         ? a.some((x) => x > 12)
             ? 1
@@ -51,21 +53,25 @@ const rowValue = (d = ",", flag = "") => {
  * Check and get the sign up date based on RegExp pattern.
  * Return as string if any matched.
  * Return as null of no matched.
- * @param {String} date input datestring
+ * @param {String | MyArray<String>} date input datestring
  * @returns {String | null}
  */
 const getSignDate = (date) => {
-    date = rgx.datePattern.exec(date);
-    return date ? date.toString() : null;
+    if (typeof date === "string") {
+        date = rgx.datePattern.exec(date);
+        return date ? date.toString() : null;
+    }
+    let [d] = date.filter((val) => rgx.datePattern.test(val));
+    return d ? d.toString() : null;
 };
 
 /**
  * Get and convert current phone number
- * @param {String | String[]} ph input phone
+ * @param {String | String[] | Number} ph input phone
  * @returns {String}
  */
 const setPhone = (ph) => {
-    ph = typeof ph === "string" ? ph : ph.join("");
+    ph = typeof ph === "string" ? ph : Array.isArray(ph) ? ph.join("") : ph.toString();
     return ph.replace(rgx.phoneValue, function (m0, g1, g2, g3) {
         let phone = g1 !== undefined ? g1 : g2.replace(/(\-| )/g, ""),
             extra = phone.charAt(0) === "8" && m0.charAt(0) !== "+" ? "62" : "";
@@ -73,4 +79,27 @@ const setPhone = (ph) => {
     });
 };
 
-export { monthIndex, setPhone, getSignDate, rowValue, useComma };
+/**
+ * Transfrom row data
+ * @param {MyArray<String>} data;
+ * @return {false | MyArray<string>}
+ */
+const transformRow = (data) => {
+    if (data.isEmpty) return false;
+
+    /** @type {(val: string) => boolean} */
+    const validPhone = (val) => rgx.phonePattern.test(val);
+    if (!data.some((val, idx) => validPhone(val) && idx !== 0)) return false;
+
+    data.forEach((e, i) => {
+        data[i] = /^"(.*)"$/.test(e) ? e.replace(/(^")|("$)/g, "") : data[i];
+        data[i] = validPhone(e)
+            ? setPhone(e)
+            : isNumeric(e)
+            ? Number(e).toString()
+            : data[i];
+    });
+    return data;
+};
+
+export { monthIndex, setPhone, getSignDate, rowValue, useComma, transformRow };
--- src/scripts/models/CSVFile.js
+++ src/scripts/models/CSVFile.js
@@ -1,13 +1,36 @@
 import MyArray from "./MyArray";
 import { rgx } from "../lib/Constant";
-import { isNumeric } from "../lib/Util";
+import { isNumeric, createFilteredObject } from "../lib/Util";
+import readXlsxFile from "../lib/Xlsx";
+import { options } from "./Settings";
 import * as util from "../lib/CSVUtil";
+
+/**
+ * @typedef {{
+ *     fname: string[];
+ *     ftype: string;
+ *     numb: number;
+ *     ext: string;
+ * }} fileName;
+ *
+ * @typedef {{
+ *     monthIndex: 0 | 1 | 2;
+ *     splitter: string
+ * }} csvOptions;
+ *
+ * @typedef {{
+ *     fileUrl: string;
+ *     fileName: string;
+ * }} fileOutputInfo;
+ */
 
 class CSVFile {
     constructor() {
         this.types = new MyArray("gagal", "error");
+        /** @type {fileName} */
         this.fileName = { fname: [], ftype: "", numb: 1, ext: ".csv" };
-        this.options = {};
+        /** @type {csvOptions} */
+        this.options = createFilteredObject(options.default, ["monthIndex", "splitter"]);
     }
 
     /**
@@ -16,12 +39,13 @@ class CSVFile {
      */
     async import(file) {
         if (!file) return this;
-        const data = await file.text();
+        const data = await ((f) => {
+            return f.type === "text/csv" ? f.text() : f.arrayBuffer();
+        })(file);
+
         if (!data) return this;
-
         this.constructFileName(file.name);
-
-        return this.readData(data.split(/\r\n|\r|\n/), file);
+        return this.readData(data);
     }
 
     /**
@@ -32,137 +56,159 @@ class CSVFile {
         let [name, ext] = filename.split(".");
         const newName = { fname: [], ftype: "", numb: 1, ext: `.${ext || "csv"}` };
         name.replace(rgx.forFilename, (m, t, n, f) => {
-            if (f !== undefined) {
-                newName.fname.push(f);
-            }
-            if (t !== undefined && this.types.isOnArray(t)) {
-                newName.ftype = t || "";
-            }
-            if (n !== undefined) {
-                newName.numb = isNumeric(n) ? Number(n) : 1;
-            }
-
+            if (f !== undefined) newName.fname.push(f);
+            if (t !== undefined && this.types.isOnArray(t)) newName.ftype = t || "";
+            if (n !== undefined) newName.numb = isNumeric(n) ? Number(n) : 1;
             return "";
         });
         this.fileName = Object.assign({}, this.fileName, newName);
     }
 
     /**
-     * Load file from text based filetype (txt/csv)
-     * @param {Array} data csv|text file array of row data
+     * Load file from text based filetype (csv/xls)
+     * @param {String | ArrayBuffer} data String text or ArrayBuffer data file
      * @returns
      */
-    readData(data) {
-        /**
-         * Detect and get csv row data, then convert it into custom Array.
-         * If it's invalid row, then return false.
-         * @param {String} row CSV row data
-         * @param {String} splitter csv delimiter
-         * @returns {MyArray | False}
-         */
-        const isValidRow = (row, splitter) => {
-            const data = this.toArray(row, splitter),
-                validPhone = (val) => rgx.phonePattern.test(val);
-            if (row != "" && data && data.length >= 2) {
-                if (!data.some((val, idx) => validPhone(val) && idx !== 0)) return false;
-                data.forEach((a, i) => {
-                    data[i] = /^"(.*)"$/.test(a) ? a.replace(/(^")|("$)/g, "") : data[i];
-                    data[i] = validPhone(a)
-                        ? util.setPhone(a)
-                        : isNumeric(a)
-                        ? Number(a).toString()
-                        : data[i];
-                });
-                return data;
-            }
-            return false;
-        };
+    async readData(data) {
+        /** @type {String[]|MyArray<String>[]} */
+        const dataArr =
+            typeof data === "string"
+                ? data.split(/\r\n|\r|\n/)
+                : await this.handleExcel(data);
 
-        const idx = data.length > 1 && data[1] != "" ? 1 : 0,
-            newData = new MyArray(),
-            dt = new MyArray(),
-            splitter = util.useComma(data[idx]) ? "," : ";";
+        const splitter = (([d, ...rest]) => {
+            d = d || rest[0];
+            if (typeof d !== "string") return false;
+            return util.useComma(d) ? "," : ";";
+        })(dataArr);
 
-        data.forEach((e) => {
-            let row;
-            if ((row = isValidRow(e, splitter))) {
+        /** @type {MyArray<String>[]} */
+        const newData = new MyArray();
+        /** @type {MyArray<String>} */
+        const dt = new MyArray();
+
+        dataArr.forEach((e) => {
+            let row = ((e) => {
+                let val = Array.isArray(e) ? e : this.toArray(e, splitter);
+                return util.transformRow(val);
+            })(e);
+            if (row) {
                 newData.push(row);
-                let s;
-                if ((s = util.getSignDate(row))) dt.push(s);
+                let date = util.getSignDate(e);
+                if (date) dt.push(date);
             }
         });
 
         this.options = Object.assign({}, this.options, {
             monthIndex: dt.isEmpty ? false : util.monthIndex(dt),
-            splitter: splitter,
+            splitter: splitter || this.options.splitter,
         });
 
-        this.data = newData;
+        if (splitter) options.setOption("splitter", splitter);
+
+        this.data = newData.filter((e) => !e.isEmpty);
+        // console.log(newData, dt, this);
 
         return this;
+    }
+
+    /**
+     * Handle excel file and return it as Array of rows
+     * @param {ArrayBuffer} file
+     * @param {import("./../lib/Xlsx/modules/parser").baseOptions?} options
+     * @return {Promise<Array>}
+     */
+    async handleExcel(file, options = {}) {
+        let opt = Object.assign({}, { sheet: 1 }, options);
+        return await readXlsxFile(file, opt);
     }
 
     /**
      * Just Convert csv data into custom Array.
      * @param {String} text csv row value
      * @param {String} delimiter csv delimiter
-     * @returns {MyArray}
+     * @returns {MyArray<string>}
      */
     toArray(text, delimiter = ",") {
+        /**
+         * @type {MyArray<string>}
+         */
         let data = new MyArray();
-        text.replace(util.rowValue(delimiter, "g"), function (match, g1, g2, g3) {
-            if (g1 !== undefined) {
-                data.push(g1.replace(/\\'/g, "'"));
-            } else if (g2 !== undefined) {
-                data.push(g2.replace(/\\"/g, '"'));
-            } else if (g3 !== undefined) {
-                data.push(g3.replace(/^['"]?(.*)['"]?$/, `$1`));
-            }
+        /**
+         * @type {{(m:string, ...groups:string[]):string}}
+         */
+        const replacer = (m, ...groups) => {
+            groups.forEach((v, i) => {
+                if (i >= groups.length - 2) return;
+                const { rgx, val } = ((i) => {
+                    return {
+                        rgx: [/\\'/g, /\\"/g, /^['"]?(.*)['"]?$/][i],
+                        val: ["'", '"', `$1`][i],
+                    };
+                })(i);
+                if (!!v) data.push(v.replace(rgx, val));
+            });
             return "";
-        });
+        };
+        text.replace(util.rowValue(delimiter, "g"), replacer);
+
         if (/,\s*$/.test(text)) data.push("");
-        return data;
+        return data.nonEmptyValue;
     }
 
     /**
      * Export array to CSV File based on current file
      * @param {String} type error type
      * @param {Array | MyArray} data
-     * @returns
+     * @returns {fileOutputInfo}
      */
     export(type, data) {
-        /**
-         * Proceed array to Blob data
-         */
-        const csvData = ((data) => {
-            function proceedRow(row) {
-                let finalVal = "";
-                for (let j = 0; j < row.length; j++) {
-                    let innerValue = row[j] === null ? "" : row[j].toString();
-                    if (row[j] instanceof Date) {
-                        innerValue = row[j].toLocaleString();
-                    }
-                    let result = innerValue.replace(/"/g, '""');
-                    if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
-                    if (j > 0) finalVal += ",";
-                    finalVal += result;
-                }
-                return finalVal + "\n";
-            }
+        const { splitter } = this.options;
+        // /**
+        //  * Proceed array to Blob data
+        //  */
+        // const csvData = ((data, splitter) => {
+        //     /**
+        //      * Format row value to string
+        //      * @param {string | Date | null} val
+        //      * @returns {string}
+        //      */
+        //     const formatRowValue = (val) => {
+        //         val = val
+        //             ? val instanceof Date
+        //                 ? val.toLocaleString()
+        //                 : val.toString()
+        //             : "";
+        //         let result = val.replace(/"/g, '""');
+        //         if (result.search(/("|,|\n)/g) >= 0) result = `"${result}"`;
+        //         return result;
+        //     };
+        //     /**
+        //      * Conver an array of row to csv row string
+        //      * @param {MyArray | Array } row
+        //      * @returns {String}
+        //      */
+        //     const proceedRow = (row) => {
+        //         let delimiter = splitter || ",",
+        //             finalVal = row.map(formatRowValue).join(delimiter);
 
-            let csvData = "";
-            for (let i = 0; i < data.length; i++) {
-                csvData += proceedRow(data[i]);
-            }
+        //         return `${finalVal}\n`;
+        //     };
 
-            return new Blob([csvData], { type: "text/csv;charset=utf-8;" });
-        })(data);
+        //     let csvData = "";
+        //     data.forEach((e) => (csvData += proceedRow(e)));
+        //     // for (let i = 0; i < data.length; i++) {
+        //     //     csvData += proceedRow(data[i]);
+        //     // }
 
-        /**
-         * Create new file name to export
-         */
+        //     return new Blob([csvData], { type: "text/csv;charset=utf-8;" });
+        // })(data, splitter);
+
+        // /**
+        //  * Create new file name to export
+        //  */
         const newName = ((type) => {
-            let { fname, ext } = this.fileName;
+            let { fname } = this.fileName;
 
             const getNumb = () => {
                 let { ftype, numb } = this.fileName;
@@ -171,39 +217,92 @@ class CSVFile {
                 return `_${numb + 1}`;
             };
 
-            return `${fname.join("_")}_${type}${getNumb()}${ext}`;
+            return `${fname.join("_")}_${type}${getNumb()}.csv`;
         })(type);
 
-        return { fileUrl: URL.createObjectURL(csvData), fileName: newName };
+        // return { fileUrl: URL.createObjectURL(csvData), fileName: newName };
+        return CSVFile.createFile(newName, data, splitter);
     }
 
-    static createFile(name, data) {
+    /**
+     * Create a csv file from array data
+     * @param {String} name
+     * @param {MyArray | Array} data
+     * @param {String?} splitter
+     * @returns {fileOutputInfo}}
+     */
+    static createFile(name, data, splitter = ",") {
         /**
          * Proceed array to Blob data
          */
-        const csvData = ((data) => {
-            function proceedRow(row) {
-                let finalVal = "";
-                for (let j = 0; j < row.length; j++) {
-                    let innerValue = row[j] === null ? "" : row[j].toString();
-                    if (row[j] instanceof Date) {
-                        innerValue = row[j].toLocaleString();
-                    }
-                    let result = innerValue.replace(/"/g, '""');
-                    if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
-                    if (j > 0) finalVal += ",";
-                    finalVal += result;
-                }
-                return finalVal + "\n";
-            }
+        const csvData = ((data, splitter) => {
+            /**
+             * Format row value to string
+             * @param {string | Date | null} val
+             * @returns {string}
+             */
+            const formatRowValue = (val) => {
+                val = val
+                    ? val instanceof Date
+                        ? val.toLocaleString()
+                        : val.toString()
+                    : "";
+                let result = val.replace(/"/g, '""');
+                if (result.search(/("|,|\n)/g) >= 0) result = `"${result}"`;
+                return result;
+            };
+
+            /**
+             * Conver an array of row to csv row string
+             * @param {MyArray | Array } row
+             * @returns {String}
+             */
+            const proceedRow = (row) => {
+                let delimiter = splitter || ",",
+                    finalVal = row.map(formatRowValue).join(delimiter);
+
+                return `${finalVal}\n`;
+            };
 
             let csvData = "";
-            for (let i = 0; i < data.length; i++) {
-                csvData += proceedRow(data[i]);
-            }
+            data.forEach((e) => (csvData += proceedRow(e)));
+            // for (let i = 0; i < data.length; i++) {
+            //     csvData += proceedRow(data[i]);
+            // }
 
             return new Blob([csvData], { type: "text/csv;charset=utf-8;" });
-        })(data);
+        })(data, splitter);
+        /**
+         * Proceed array to Blob data
+         */
+        // const csvData = ((data) => {
+        //     /**
+        //      * Conver an array row to csv string row
+        //      * @param {Array | MyArray} row
+        //      * @returns {String}
+        //      */
+        //     function proceedRow(row) {
+        //         let finalVal = "";
+        //         for (let j = 0; j < row.length; j++) {
+        //             let innerValue = row[j] === null ? "" : row[j].toString();
+        //             if (row[j] instanceof Date) {
+        //                 innerValue = row[j].toLocaleString();
+        //             }
+        //             let result = innerValue.replace(/"/g, '""');
+        //             if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
+        //             if (j > 0) finalVal += ",";
+        //             finalVal += result;
+        //         }
+        //         return finalVal + "\n";
+        //     }
+
+        //     let theData = "";
+        //     for (let i = 0; i < data.length; i++) {
+        //         theData += proceedRow(data[i]);
+        //     }
+
+        //     return new Blob([theData], { type: "text/csv;charset=utf-8;" });
+        // })(data);
 
         return { fileUrl: URL.createObjectURL(csvData), fileName: `${name}.csv` };
     }
