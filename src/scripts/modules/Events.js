3413e5702833133f2e12b8c00e60341cc20e5aeb
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2023-02-02 14:43:27 +0700

    feat(xlsx): Implement export as xlsx on downloading group contacts

--- src/scripts/modules/Events.js
+++ src/scripts/modules/Events.js
@@ -1,5 +1,6 @@
 import MyDate from "../models/MyDate";
 import { Debug } from "./Debug";
+import { svgData } from "../lib/Constant";
 import { waydown } from "./Waydown";
 import { options } from "../models/Settings";
 import { modal } from "../models/Modals";
@@ -12,9 +13,15 @@ import { chat } from "../models/Chatrooms";
 import { message } from "../models/Messages";
 import { DOM } from "../lib/HtmlModifier";
 import CSVFile, { csvFile } from "../models/CSVFile";
-import { dateFormat, isNumeric } from "../lib/Util";
+import { dateFormat, isNumeric, titleCase } from "../lib/Util";
 import MyArray from "../models/MyArray";
-import { loadRecipient, resetRecipient, checkStatus, startProcess } from "./Main";
+import {
+    loadRecipient,
+    resetRecipient,
+    checkStatus,
+    startProcess,
+    exportDataToFile,
+} from "./Main";
 
 /**
  * A bunch of EventListener
@@ -115,12 +122,16 @@ import { loadRecipient, resetRecipient, checkStatus, startProcess } from "./Main
      * @param {Event} e Event
      */
     updateText(e) {
-        const { id, value, innerText } = e.target || e.currentTarget,
-            key = id === "message" ? "Message" : "Caption",
-            props = {};
+        const { id, value, innerText } = e.target || e.currentTarget;
+        const key = ((i) => {
+            let strs = `${i}`.match(/[A-Za-z]+/g);
+            if (!strs) return `input${titleCase(i)}`;
 
-        props[`input${key}`] = value || innerText;
-        message.setProperties(props);
+            strs.forEach((e, idx) => (strs[idx] = titleCase(e)));
+            return `input${strs.join()}`;
+        })(id);
+
+        message.setProperties({ [key]: value || innerText }, false);
     }
 
     /**
@@ -312,6 +323,22 @@ import { loadRecipient, resetRecipient, checkStatus, startProcess } from "./Main
                     options.setOption(id, val);
                 })(id, value);
                 break;
+            case "exportType":
+                (async function (id, val) {
+                    options.setOption(id, val);
+                    let downloadBtn = await DOM.hasElement("span.wfu-link");
+                    if (downloadBtn) {
+                        // let el = DOM.getElement("span.wfu-link");
+                        let title = (({ title: t }) => {
+                            t = t.replace(/(.*)\.[^.]+(["]+)$/g, function (m, g1, g2) {
+                                return `${g1}.${val + g2}`;
+                            });
+                            return t;
+                        })(downloadBtn);
+                        DOM.setElement(downloadBtn, { title: title });
+                    }
+                })(id, value);
+                break;
         }
     }
 
@@ -352,26 +379,23 @@ import { loadRecipient, resetRecipient, checkStatus, startProcess } from "./Main
             chatMenu = DOM.getElement(`.${chatHeader} .${menu}.${menuDefault}`),
             downloadMenu = DOM.getElement("span[data-icon='download-alt']", chatMenu);
 
-        const createDonwloadBtn = (data) => {
-            const downloadBtn = (({ fileName }) => {
-                let ico = DOM.createSVGElement({
-                    size: "24 24",
-                    fill: "currentColor",
-                    d: [
-                        "M18.948 11.112C18.511 7.67 15.563 5 12.004 5c-2.756 0-5.15 1.611-6.243 4.15-2.148.642-3.757 2.67-3.757 4.85 0 2.757 2.243 5 5 5h1v-2h-1c-1.654 0-3-1.346-3-3 0-1.404 1.199-2.757 2.673-3.016l.581-.102.192-.558C8.153 8.273 9.898 7 12.004 7c2.757 0 5 2.243 5 5v1h1c1.103 0 2 .897 2 2s-.897 2-2 2h-2v2h2c2.206 0 4-1.794 4-4a4.008 4.008 0 0 0-3.056-3.888z",
-                        "M13.004 14v-4h-2v4h-3l4 5 4-5z",
-                    ],
+        /** @type {(filename: string) => HTMLElement} */
+        const createDonwloadBtn = (filename) => {
+            const downloadBtn = ((name) => {
+                let ico = DOM.createSVGElement(svgData.groupDownloadBtnSvg, {
+                    width: "24",
+                    height: "24",
                 });
 
                 return DOM.createElement({
                     tag: "span",
-                    title: `Download "${fileName}"`,
+                    title: `Download "${name}"`,
                     "data-testid": "download-alt",
                     "data-icon": "download-alt",
                     classid: "wfu-link",
                     html: ico.outerHTML,
                 });
-            })(data);
+            })(filename);
 
             return ((downloadBtn) => {
                 let btn = DOM.createElement({
@@ -406,19 +430,15 @@ import { loadRecipient, resetRecipient, checkStatus, startProcess } from "./Main
                 contacts.push([pushname || name || user, user]);
             }
 
-            let data = CSVFile.createFile(subject, contacts),
-                btn = createDonwloadBtn(data);
+            // let { fileUrl, fileName } = CSVFile.createFile(subject, contacts),
+            //     fname = exportType === "csv" ? `${fileName}.csv` : `${subject}.xlsx`;
+            let btn = createDonwloadBtn(subject);
 
             if (!downloadMenu) {
                 chatMenu.insertBefore(btn, chatMenu.firstChild);
             } else if (downloadMenu && e.target === downloadMenu) {
                 if (await user.check()) {
-                    let { fileUrl, fileName } = data;
-                    DOM.createElement({
-                        tag: "a",
-                        href: fileUrl,
-                        download: `${fileName}`,
-                    }).click();
+                    return exportDataToFile(contacts, subject);
                 }
             }
         }
