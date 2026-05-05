ff3f774b137716b9d42e3f9ab4c765d6f5a0520a
Rizal Nurhidayat <rizal.nurhidayat96@gmail.com>
2026-05-06 01:11:46 +0700

    chore(build): build & bump new version

--- update.meta.js
+++ update.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         WayFu Dev
 // @description  WhatsApp Easy Follow Up.
-// @date         2026-05-04
+// @date         2026-05-05
 // @copyright    2018, Rizal Nurhidayat (https://github.com/rzlnhd)
 // @license      CC BY-NC-ND 4.0; https://creativecommons.org/licenses/by-nc-nd/4.0/
 // @icon         https://wayfu.id/v4/assets/wayfu-icon.webp
 // @homepage     https://wayfu.id/
 // @supportURL   https://wayfu.id/
-// @version      4.7.4
+// @version      4.7.5
 // @author       Rizal Nurhidayat (https://github.com/rzlnhd)
 // @match        https://web.whatsapp.com/*
 // @grant        GM_addStyle
